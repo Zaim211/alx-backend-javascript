baa3e98382720eadb393a502df566780a1ebda80
Zaim211 <yousseftitazaim@gmail.com>
2024-03-05 22:15:13 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,10 @@ export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
 
-  return Promise.all([promise1, promise2]).then((res) => {
-    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
-  }).catch(() => console.log('Signup system offline'));
+  return Promise
+    .all([promise1, promise2])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
