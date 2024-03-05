e11998f6ee8309d1d63a4cf6b84ca6cab295188e
Zaim <Yousseftitazaim@gmail.com>
2024-03-05 19:37:06 +0000

    Delete 0x00-ES6_basic directory

--- 0x00-ES6_basic/100-createIteratorObject.js
+++
@@ -1,9 +0,0 @@
-export default function createIteratorObject(report) {
-  const Names = [];
-  for (const item of Object.keys(report.allEmployees)) {
-    for (const name of report.allEmployees[item]) {
-      Names.push(name);
-    }
-  }
-  return Names;
-}
