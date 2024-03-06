1d97eceb9416bfe7bb3d94e9ba27e6066b942664
Zaim211 <yousseftitazaim@gmail.com>
2024-03-06 19:30:22 +0000

    Await / Async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,11 +2,11 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default async function asyncUploadUser() {
   try {
-    const promise1 = await uploadPhoto(photo);
-    const promise2 = await createUser(user);
+    const promise1 = await uploadPhoto();
+    const promise2 = await createUser();
 
-   return(promise1, promise2);
-  } catch(error) {
+   return({ promise1: photo, promise2: user });
+  } catch (error) {
     return ({ photo: null, user: null });
   }
 }
