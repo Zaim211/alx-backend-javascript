be7c89094cb203b20d2e5159eb35c9516ddd7756
Zaim211 <yousseftitazaim@gmail.com>
2024-03-07 22:18:40 +0000

    Primitive - Holberton Class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,17 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number' || typeof location !== 'string') throw new Error();
+      this._size = size;
+      this._location = location;
+  }
+
+  [Symbol.toPrimitive](value) {
+    if (value === 'string') {
+      return this._location;
+    }
+    if (value === 'number') {
+      return this._size;
+    }
+    return this;
+  }
+}
