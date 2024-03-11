68e8d3bb55af19e39ae32b98f469153b6f6d9dfd
Zaim211 <yousseftitazaim@gmail.com>
2024-03-11 21:45:38 +0000

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+  if (!startString.length || !startString) {
+    return result;
+  }
+  for (const element of set) {
+    if (element && element.startsWith(startString)) {
+      result += `${element.slice(startString.length)}-`;
+    }
+  }
+  return result.slice(0, result.length - 1);
+}
