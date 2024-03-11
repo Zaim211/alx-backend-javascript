609ffa7f3ab554524dcc916dd80dc43c706517c8
Zaim211 <yousseftitazaim@gmail.com>
2024-03-11 22:57:57 +0000

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+const weakMap = new WeakMap();
+export { weakMap };
+
+export default function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    const count = weakMap.get(endpoint);
+    if (count >= 4) throw new Error('Endpoint load is high');
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+}
