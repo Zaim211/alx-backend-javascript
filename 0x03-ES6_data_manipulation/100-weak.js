f990cd2999c2cb221cbb50569b335b2d0bae2a46
Zaim211 <yousseftitazaim@gmail.com>
2024-03-12 22:19:10 +0000

    ES6 data manipulation, Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,9 +2,9 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   let result = weakMap.get(endpoint) || 0;
-    result++;
+
+  result++;
+
   weakMap.set(endpoint, result);
-  if (result >= 5) {
-    throw new Error('Endpoint load is high');
-  }
-};
+  if (result >= 5) throw new Error('Endpoint load is high');
+}
