42dfd68424823b912752bc8e439e339d39ef10c0
Zaim211 <yousseftitazaim@gmail.com>
2024-04-29 18:54:21 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
-  process.stdin.on('agrs', (args) => {
-    process.stdout.write(`Your name is: ${args.toString()}`);
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
 } else {
-  process.stdin.on('agrs', (args) => {
-    process.stdout.write(`Your name is: ${args.toString()}`);
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
-  process.on('end', () => {
+  process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
   });
 }
