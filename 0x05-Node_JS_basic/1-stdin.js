a9ad4671ec9df5346c8f5fdd4e380030a75077a3
Zaim211 <yousseftitazaim@gmail.com>
2024-04-29 19:17:05 +0000

    Fixed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,8 @@
+#!usr/bin/node
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+// isTTY is a property of the process.stdout and process.stdin streams.
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
