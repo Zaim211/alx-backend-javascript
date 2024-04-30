5249ab3399e72687cfb9877731d3c2f85287163d
Zaim211 <yousseftitazaim@gmail.com>
2024-04-30 21:15:58 +0000

    Done

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const { readFile } = require('fs');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+function countStudents(filepath) {
+  const fields = {};
+  return new Promise((resolve, reject) => {
+    readFile(filepath, 'utf-8', ((error, dataStudents) => {
+      if (error) {
+        const err = new Error('Cannot load the database');
+	reject(err);
+      } else {
+	let outp = '';
+        const eachline = dataStudents.split('\n').filter((l) => l !== '').slice(1);
+        const items = eachline.map(l => l.split(','));
+
+        outp += `Number of students: ${items.length}`;
+
+        items.forEach((item) => {
+          if (item.length === 4) {
+            const [firstName, lastName, age, field] = item;
+	    if (fields[field]) {
+              fields[field].push(firstName);
+	    } else {
+	      fields[field] = [firstName];
+	    }
+          }
+        });
+
+        for (const field in fields) {
+          if (Object.hasOwnProperty.call(fields, field)) {
+            const names = fields[field].join(', ');
+            const count = fields[field].length;
+            outp += `\nNumber of students in ${field}: ${count}. List: ${names}`;
+          }
+        }
+
+        resolve(outp);
+      }
+    }));
+  })
+};
+
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((data) => res.send(`This is the list of our students\n${data}`))
+    .catch((error) => res.send(`This is the list of our students\n${error.message}`));
+});
+
+app.listen(1245);
+
+module.exports = app;
