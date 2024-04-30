867f7977415172b15ec1e905bcededb95a267890
Zaim211 <yousseftitazaim@gmail.com>
2024-04-30 23:32:24 +0000

    Fix errors

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,14 +1,14 @@
-const readDatabase = require('../utils');
+import readDatabase from '../utils';
 
 class StudentsController {
   static getAllStudents(request, response) {
-    readDatabase(process.argv[2]).then((output) => {
+    readDatabase(process.argv[2]).then((fields) => {
       const outp = [];
       outp.push('This is the list of our students');
-      const key = Object.keys(output);
+      const key = Object.keys(fields);
       key.sort();
       for (let k = 0; k < key.length; k += 1) {
-        outp.push(`\nNumber of students in ${key[k]}: ${output[key[k]].length}. List: ${output[key[k]].join(' ,')}`);
+        outp.push(`\nNumber of students in ${key[k]}: ${fields[key[k]].length}. List: ${fields[key[k]].join(' ,')}`);
       }
       response.status(200).send(outp.join('\n'));
     }).catch(() => {
@@ -18,9 +18,9 @@ class StudentsController {
 
   static getAllStudentsByMajor(request, response) {
     const field = request.params.major;
-    readDatabase(process.argv[2]).then((output) => {
-      if (field in output) {
-        response.status(200).send(`List: ${output[field].join(', ')}`);
+    readDatabase(process.argv[2]).then((fields) => {
+      if (field in fields) {
+        response.status(200).send(`List: ${fields[field].join(', ')}`);
       } else {
 	response.status(500).send('Major parameter must be CS or SWE');
       }
@@ -30,4 +30,4 @@ class StudentsController {
   }
 }
 
-module.exports = StudentsController;
+export default StudentsController;
