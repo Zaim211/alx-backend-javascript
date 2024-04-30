f69890353b8dc84071cc69cabba6f13b81c29209
Zaim211 <yousseftitazaim@gmail.com>
2024-04-30 00:47:38 +0000

    update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,6 +1,40 @@
 const http = require('http');
 const { readFile } = require('fs');
 
+function countStudents(filepath) {
+  const fields = {};
+  return new Promise((resolve, reject) => {
+    readFile(filepath, ((error, dataStudents) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const eachline = dataStudents.toString().split('\n');
+        const items = eachline.map(l => l.split(','));
+	let output = '';
+
+        items.forEach((item) => {
+          if (item.length === 4) {
+            const [firstName, lastName, age, field] = item;
+	    if (fields[field]) {
+              fields[field].push(firstName);
+	    } else {
+	      fields[field] = [firstName];
+	    }
+          }
+        });
+	output += `Number of students: ${items.length}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${items[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
+      }
+    }));
+  })
+};
+
 const app = http.createServer((req, res) => {
   const url = req.url;
   if (url === '/') {
@@ -9,51 +43,17 @@ const app = http.createServer((req, res) => {
     res.end();
   }
   if (url === '/students') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('This is the list of our students');
-    function countStudents(filepath) {
-      const fields = {};
-      return new Promise((resolve, reject) => {
-        readFile(filepath, 'utf-8', ((error, dataStudents) => {
-          if (error) {
-            reject(Error('Cannot load the database'));
-          } else {
-            const eachline = dataStudents.split('\n').filter((l) => l !== '').slice(1);
-            const items = eachline.map(l => l.split(','));
-
-            console.log(`Number of students: ${items.length}`);
-            items.forEach((item) => {
-              if (item.length === 4) {
-                const [firstName, lastName, age, field] = item;
-	        if (fields[field]) {
-                  fields[field].push(firstName);
-	        } else {
-	          fields[field] = [firstName];
-	        }
-              }
-            });
-
-            for (const field in fields) {
-              if (Object.hasOwnProperty.call(fields, field)) {
-                const names = fields[field].join(', ');
-                const count = fields[field].length;
-                console.log(`Number of students in ${field}: ${count}. List: ${names}`);
-              }
-            }
-
-            resolve();
-          }
-        }));
-      })
-      countStudents(process.argv[2].toString()).then((output) => {
-        const outString = output.slice(0, -1);
-        response.end(outString);
-      }).catch(() => {
-        response.statusCode = 404;
-        response.end('Cannot load the database');
-      });
-    };
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      res.end(outString);
+    }).catch(() => {
+      res.statusCode = 404;
+      res.end('Cannot load the database');
+    });
+  };
 });
+
 
 app.listen(1245);
 
