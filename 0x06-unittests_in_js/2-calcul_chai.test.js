03ef0ac3083d6c4d790aa9542664747f8ab37a69
Zaim211 <yousseftitazaim@gmail.com>
2024-05-01 17:13:00 +0000

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,88 +1,88 @@
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 const chai = require("chai");
 const expect = chai.expect;
 
 describe("calculateNumber, type `SUM`", function() {
   it("checks sum round numbers", function() {
-    except(calculateNumber('SUM', 2, 2)).to.equal(4);
+    expect(calculateNumber('SUM', 2, 2)).to.equal(4);
   });
 
   it("Test numbers are rounded", function() {
-    except(calculateNumber('SUM', 4.49, 3.7)).to.equal(8);
+    expect(calculateNumber('SUM', 4.49, 3.7)).to.equal(8);
   });
 
   it("Test numbers are rounded", function() {
-    except(calculateNumber('SUM', 3.3, 2.49)).to.equal(5);
+    expect(calculateNumber('SUM', 3.3, 2.49)).to.equal(5);
   });
 
   it("Test numbers are rounded", function() {
-    except(calculateNumber('SUM', -8.2, 2)).to.equal(-6);
+    expect(calculateNumber('SUM', -8.2, 2)).to.equal(-6);
   });
 
   it("Test numbers are rounded", function() {
-    except(calculateNumber('SUM', 0, 0)).to.equal(0);
+    expect(calculateNumber('SUM', 0, 0)).to.equal(0);
   });
 
   it("Test numbers are rounded", function() {
-    except(calculateNumber('SUM', -3.7, -2)).to.equal(-6);
+    expect(calculateNumber('SUM', -3.7, -2)).to.equal(-6);
   });
 
 });
 
 describe("calculateNumber, type `SUBTRACT`", function() {
   it("checks Subtract round numbers", function() {
-    except(calculateNumber('SUBTRACT', 2, 2)).to.equal(0);
+    expect(calculateNumber('SUBTRACT', 2, 2)).to.equal(0);
   });
 
   it("Test Subtract numbers are rounded", function() {
-    except(calculateNumber('SUBTRACT', 1.49, 3.7)).to.equal(-3);
+    expect(calculateNumber('SUBTRACT', 1.49, 3.7)).to.equal(-3);
   });
 
   it("Test Subtract numbers are rounded", function() {
-    except(calculateNumber('SUBTRACT', 3.3, 2.49)).to.equal(1);
+    expect(calculateNumber('SUBTRACT', 3.3, 2.49)).to.equal(1);
   });
 
   it("Test Subtract numbers are rounded", function() {
-    except(calculateNumber('SUBTRACT', -8.2, 2)).to.equal(-10);
+    expect(calculateNumber('SUBTRACT', -8.2, 2)).to.equal(-10);
   });
 
   it("Test Subtract numbers are rounded", function() {
-    except(calculateNumber('SUBTRACT', 1000, 1.49)).to.equal(999);
+    expect(calculateNumber('SUBTRACT', 1000, 1.49)).to.equal(999);
   });
 
   it("Test Subtract numbers are rounded", function() {
-    except(calculateNumber('SUBTRACT', -3.7, -2)).to.equal(-2);
+    expect(calculateNumber('SUBTRACT', -3.7, -2)).to.equal(-2);
   });
 
 });
 
 describe("calculateNumber, type `DIVIDE`", function() {
   it("checks Subtract round numbers", function() {
-    except(calculateNumber('DIVIDE', 2, 2)).to.equal(1);
+    expect(calculateNumber('DIVIDE', 2, 2)).to.equal(1);
   });
 
   it("Test Subtract numbers are rounded", function() {
-    except(calculateNumber('DIVIDE', 4.49, 3.7)).to.equal(1);
+    expect(calculateNumber('DIVIDE', 4.49, 3.7)).to.equal(1);
   });
 
   it("Test Subtract numbers are rounded", function() {
-    except(calculateNumber('DIVIDE', 3.3, 2.49)).to.equal(1.5);
+    expect(calculateNumber('DIVIDE', 3.3, 2.49)).to.equal(1.5);
   });
 
   it("Test Subtract numbers are rounded", function() {
-    except(calculateNumber('DIVIDE', -8.2, 2)).to.equal(-4);
+    expect(calculateNumber('DIVIDE', -8.2, 2)).to.equal(-4);
   });
 
   it("Test Subtract numbers are rounded", function() {
-    except(calculateNumber('DIVIDE', 1000, 1.49)).to.equal(1000);
+    expect(calculateNumber('DIVIDE', 1000, 1.49)).to.equal(1000);
   });
 
   it("Test Subtract numbers are rounded", function() {
-    except(calculateNumber('DIVIDE', -3.7, -2)).to.equal(2);
+    expect(calculateNumber('DIVIDE', -3.7, -2)).to.equal(2);
   });
 
   it("Test Subtract numbers are rounded", function() {
-    except(calculateNumber('DIVIDE', -3.7, 0)).to.equal('Error');
+    expect(calculateNumber('DIVIDE', -3.7, 0)).to.equal('Error');
   });
 
 });
